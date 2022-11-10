d35aacda43f01a879358e3882c4b1f3c03225786
Abu Sayeed <sayeed@pop-os.localdomain>
2022-11-10 18:01:26 +0600

    finally after 5 hours of debuging managed to solved the error!

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://assignmentDBUser:3vO2eE7385wBTkSS@cluster0.qk4n58g.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qk4n58g.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
